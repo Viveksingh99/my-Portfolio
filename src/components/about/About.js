d9af42a9bb325c5e9441850460e21d6868925ead
viveksingh99 <vivek22jan99@gmai.com>
2023-02-10 13:19:15 +0530

    frst-commit

---
+++ src/components/about/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyIHggfXo85B41VQKmu2rhVdbPrTuZpLmiFpBEsCZbJ_f1XIejsfYnQ5oOHYS8fZxwWp0&usqp=CAU"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="a-right"></div>
+    </div>
+  );
+}
+export default About
